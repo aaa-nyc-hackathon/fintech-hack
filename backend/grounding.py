9f6ac5c87d6031ebd24561375699ca1e976d4b8e
Lucas Roberts <rlucas7@Lucass-MacBook-Air.local>
2025-08-23 12:57:56 -0400

    add module for rev-ground

---
+++ backend/grounding.py
@@ -0,0 +1,12 @@
+""" grounding module
+
+    This module does a reverse grounding procedure.
+    Given the image we generate some text and take the text
+    to generate a search query. The search results are the
+    items of grounding but the generation happens beforehand
+    so this is a reverse grounding procedure. Just a term I
+    heard from Maye Dupp my friend who is also an AI researcher.
+"""
+
+
+
